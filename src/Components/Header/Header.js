d01e154ce3e4938ee71488495570832d6b223687
bedena2000 <bedena2000@gmail.com>
2022-07-02 23:16:34 +0400

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { AiOutlineMenu } from "react-icons/ai";
+import { NavLink } from "react-router-dom";
+import Logo from "../../img/logo.svg";
+import { FiSearch } from "react-icons/fi";
+import { BsCart2 } from "react-icons/bs";
+
+const Header = () => {
+  return (
+    <div className={`header ${styles.header}`}>
+      <div className={`${styles["left-part"]}`}>
+        <NavLink to="/shop">
+          <AiOutlineMenu className={`${styles["menu-icon"]}`} />
+        </NavLink>
+        <nav className="nav">
+          <NavLink className={`${styles["nav-link"]}`} to="/shop">
+            Men
+          </NavLink>
+          <NavLink className={`${styles["nav-link"]}`} to="/shop">
+            Women
+          </NavLink>
+          <NavLink className={`${styles["nav-link"]}`} to="/shop">
+            Kids
+          </NavLink>
+          <NavLink className={`${styles["nav-link"]}`} to="/shop">
+            Trending
+          </NavLink>
+        </nav>
+      </div>
+      <div className={`${styles["logo-element"]}`}>
+        <NavLink to="/">
+          <img className="logo" src={Logo} />
+        </NavLink>
+      </div>
+      <div className={`${styles["right-part"]}`}>
+        <NavLink
+          style={{ color: "#363E61" }}
+          className={`${styles["right-part-link"]}`}
+          to="/shop"
+        >
+          <FiSearch />
+        </NavLink>
+        <NavLink
+          style={{ color: "#363E61" }}
+          className={`${styles["right-part-link"]}`}
+          to="/cart"
+        >
+          <BsCart2 />
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./Main.module.css";
+import Image from "../../img/Image.png";
+import { NavLink } from "react-router-dom";
+import footerImg1 from "../../img/category1.png";
+import footerImg2 from "../../img/category2.png";
+import footerImg3 from "../../img/category3.png";
+
+const Main = () => {
+  return (
+    <>
+      <main className={`${styles.main}`}>
+        <div className={`${styles["main-left"]}`}>
+          <p className={`${styles["main-left-describe"]}`}>
+            TRENDING COLLECTION 2022
+          </p>
+          <h1 className={`${styles["main-left-title"]}`}>Fake Air Max 97</h1>
+          <p className={`${styles["main-left-description"]}`}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry'
+          </p>
+          <button className={`${styles["main-left-btn"]}`}>Shop Page</button>
+        </div>
+        <div className={`${styles["main-right"]}`}>
+          <img className={`${styles["main-right-image"]}`} src={Image} />
+        </div>
+      </main>
+      <div className={`${styles["footer"]}`}>
+        <NavLink className={`${styles["footer-item"]}`} to="/shop">
+          <img src={footerImg1} />
+        </NavLink>
+        <NavLink className={`${styles["footer-item"]}`} to="/shop">
+          <img src={footerImg2} />
+        </NavLink>
+        <NavLink className={`${styles["footer-item"]}`} to="/shop">
+          <img src={footerImg3} />
+        </NavLink>
+      </div>
+    </>
+  );
+};
+
+export default Main;
